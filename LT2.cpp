8148cb6a3838cf2171e372751ba6db49626f3080
moriics <136748652+ITIS-mphuong169@users.noreply.github.com>
2024-07-23 09:56:50 +0700

    Update LT2.cpp

--- LT2.cpp
+++ LT2.cpp
@@ -4,20 +4,26 @@
 
 using namespace std;
 
-void dfs(int node, vector<vector<int>>& adj, vector<bool>& visited) {
+void dfs(int node, vector<vector<int>>& adj, vector<bool>& visited)
+{
     visited[node] = true;
-    for (int neighbor : adj[node]) {
-        if (!visited[neighbor]) {
+    for (int neighbor : adj[node]) 
+    {
+        if (!visited[neighbor])
+        {
             dfs(neighbor, adj, visited);
         }
     }
 }
 
-int findConnectedComponents(int n, vector<vector<int>>& adj) {
+int findConnectedComponents(int n, vector<vector<int>>& adj) 
+{
     vector<bool> visited(n + 1, false);
     int components = 0;
-    for (int i = 1; i <= n; ++i) {
-        if (!visited[i]) {
+    for (int i = 1; i <= n; ++i)
+        {
+        if (!visited[i]) 
+        {
             dfs(i, adj, visited);
             components++;
         }
@@ -32,7 +38,8 @@ int main() {
     vector<vector<int>> adj(n + 1);
     set<pair<int, int>> existingEdges;
 
-    for (int i = 0; i < m; ++i) {
+    for (int i = 0; i < m; ++i)
+        {
         int a, b;
         cin >> a >> b;
         adj[a].push_back(b);
@@ -43,24 +50,18 @@ int main() {
 
     int components = findConnectedComponents(n, adj);
     int edgesNeeded = components - 1;
-
-    // T?ng s? c?p có th? ch?n
     long long totalPairs = (long long)n * (n - 1) / 2;
 
-    // T?ng s? c?p hi?n t?i
     long long existingPairs = existingEdges.size();
 
-    // S? c?p có th? ch?n d? thêm vào
     long long newPairs = totalPairs - existingPairs;
 
-    // Cách ch?n b? c?p
     long long result = 1;
-    for (int i = 0; i < edgesNeeded; ++i) {
+    for (int i = 0; i < edgesNeeded; ++i) 
+    {
         result = (result * newPairs) % P;
         newPairs--;
     }
-
     cout << result << endl;
-    return 0;
 }
 
